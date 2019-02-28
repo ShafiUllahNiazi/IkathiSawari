ee08331466d94e80f5f5c41c73d68f17d68065e7
ShafiUllahNiazi <shafiullahkhan117786@gmail.com>
2019-02-28 12:36:08 +0500

    get list of available drivers

--- IkathiSawari/app/src/main/java/com/example/shafi/ikathisawari/RiderDataCarrier.java
+++ IkathiSawari/app/src/main/java/com/example/shafi/ikathisawari/RiderDataCarrier.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,7 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,14 +39,17 @@ import java.util.List;
 import java.util.Map;
 
 public class RiderDataCarrier extends AppCompatActivity {
-
+    private static final String TAG = "RiderDataCarrier";
     Intent intent;
     Double originLat, originLong, destinationLat, destinationLong;
     private DatabaseReference mRiderAvailableRout;
     Button goBackHomeRider, getDriver;
 
-    private float radius=100;
+    private float radius = 100;
     private boolean driverFound = false;
+
+
+    ArrayList<String> availableDrivers;
 
 
     @Override
@@ -53,6 +58,8 @@ public class RiderDataCarrier extends AppCompatActivity {
         setContentView(R.layout.activity_rider_data_carrier);
         goBackHomeRider = findViewById(R.id.goBackHomeRider);
         getDriver = findViewById(R.id.getDriver);
+
+        availableDrivers = new ArrayList<>();
 
         intent = getIntent();
 
@@ -74,7 +81,7 @@ public class RiderDataCarrier extends AppCompatActivity {
         goBackHomeRider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RiderDataCarrier.this,Rider_Home.class));
+                startActivity(new Intent(RiderDataCarrier.this, Rider_Home.class));
             }
         });
 
@@ -90,29 +97,29 @@ public class RiderDataCarrier extends AppCompatActivity {
     private void getNearbyDriver() {
         final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Available Routs");
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (final DataSnapshot child : dataSnapshot.getChildren()) {
-                    databaseReference.child(child.getKey()).addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            DriverRoutInfo driverRoutInfo = dataSnapshot.getValue(DriverRoutInfo.class);
-                            Log.d("myFetchRoute ",driverRoutInfo.toString());
-                            Log.d("myFetchRoute111 ",driverRoutInfo.getRoutes().toString());
-//                            Toast.makeText(RiderDataCarrier.this, ""+child.getKey(), Toast.LENGTH_SHORT).show();
 
-                            fetchNearbyPoint(driverRoutInfo);
-                        }
+                Map<String,Object> myDrivers = (Map<String, Object>) dataSnapshot.getValue();
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                for (String driver: myDrivers.keySet()){
+                    Log.d(TAG, "driver- " + myDrivers.get(driver) );
 
-                        }
-                    });
-//                    Toast.makeText(RiderDataCarrier.this, ""+child.getKey().toString(), Toast.LENGTH_SHORT).show();
+                    Map<String,Object> dd = (Map<String, Object>) myDrivers.get(driver);
+
+                    DriverRoutInfo driverRoutInfo = new DriverRoutInfo();
+
+                    driverRoutInfo.setRoutes((List<List<HashMap<String, String>>>) dd.get("routes"));
+                    Log.d(TAG, "onDataChange: "+dd.get("routes").toString());
+                    String availableDriver=fetchNearbyPoint(driver, driverRoutInfo);
+
+
+                    availableDrivers.add(availableDriver);
                 }
 
+
+                Log.d(TAG, "Driver list size" + availableDrivers.size());
             }
 
             @Override
@@ -122,73 +129,45 @@ public class RiderDataCarrier extends AppCompatActivity {
         });
 
 
-//        float[] distance = new float[1];
-//        Location.distanceBetween(32.5839229, 71.53702969999999, 32.6817694, 71.7911439, distance);
-//
-//        Toast.makeText(this, distance[0]+" GG", Toast.LENGTH_SHORT).show();
 
-//        String rider_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
-//        DatabaseReference driverLocation = FirebaseDatabase.getInstance().getReference().child("Available Routs");
-//
-//        GeoFire geoFire = new GeoFire(driverLocation);
-//
-//
-//
-//        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(originLat, originLong),radius);
-//        geoQuery.removeAllListeners();
-//        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
-//            @Override
-//            public void onKeyEntered(String key, GeoLocation location) {
-//                driverFound = true;
-//
-//            }
-//
-//            @Override
-//            public void onKeyExited(String key) {
-//
-//            }
-//
-//            @Override
-//            public void onKeyMoved(String key, GeoLocation location) {
-//
-//            }
-//
-//            @Override
-//            public void onGeoQueryReady() {
-//                if(!driverFound){
-//
-//                    radius++;
-//                    getNearbyDriver();
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onGeoQueryError(DatabaseError error) {
-//
-//            }
-//        });
-
-
-
-
-
-
-
-
-
-
-
-//        Toast.makeText(this, "GG "+ driverLocation, Toast.LENGTH_SHORT).show();
-
-//        driverLocation.addValueEventListener(new ValueEventListener() {
+//        databaseReference.addValueEventListener(new ValueEventListener() {
 //            @Override
 //            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                dataSnapshot.child("").child("").;
+//                for (final DataSnapshot child : dataSnapshot.getChildren()) {
+//                    databaseReference.child(child.getKey()).addValueEventListener(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                            DriverRoutInfo driverRoutInfo = dataSnapshot.getValue(DriverRoutInfo.class);
+//                            Log.d("myFetchRoute ", driverRoutInfo.toString());
+//                            Log.d("myFetchRoute111 ", driverRoutInfo.getRoutes().toString());
+////                            Toast.makeText(RiderDataCarrier.this, ""+child.getKey(), Toast.LENGTH_SHORT).show();
 //
-//                for (DataSnapshot child : dataSnapshot.getChildren()) {
-//                    Toast.makeText(RiderDataCarrier.this, ""+child.getKey().toString(), Toast.LENGTH_SHORT).show();
-//                    DatabaseReference driverLocation2 = FirebaseDatabase.getInstance().getReference().child("Available Routs").child(child.getKey());
-//                    driver(driverLocation2);
+//                            String availableDriver=fetchNearbyPoint(child.getKey(), driverRoutInfo);
+//
+//                            Log.d("availableDriver ","returned "+availableDriver);
+//                            availableDrivers.add(availableDriver);
+//
+//                            Log.d("TESTER", "onDataChange: size - "+availableDriver.length());
+//
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                        }
+//                    });
+////                    Toast.makeText(RiderDataCarrier.this, ""+child.getKey().toString(), Toast.LENGTH_SHORT).show();
+//                }
+//
+//
+//
+//                Log.d("availableDriver", "Driver list size" + availableDrivers.size());
+//
+//                for (int i = 0; i < availableDrivers.size(); i++) {
+//
+//                    Log.d("availableDriver", "Driver " + i + " " + availableDrivers.get(i));
+//                    Toast.makeText(RiderDataCarrier.this, "Driver " + i + " " + availableDrivers.get(i), Toast.LENGTH_SHORT).show();
 //                }
 //
 //            }
@@ -200,20 +179,18 @@ public class RiderDataCarrier extends AppCompatActivity {
 //        });
 
 
-
-
     }
 
-    private void fetchNearbyPoint(DriverRoutInfo driverRoutInfo) {
+    private String fetchNearbyPoint(String driverKey, DriverRoutInfo driverRoutInfo) {
 
         boolean originFound = false;
         boolean destinationFound = false;
         LatLng originFoundPoint = null;
-        LatLng destinationFoundPoint= null;
+        LatLng destinationFoundPoint = null;
         float originFoundPointDistance = radius;
-        float destinationFoundPointDistance= radius;
+        float destinationFoundPointDistance = radius;
 
-        List<List<HashMap<String, String>>> result= driverRoutInfo.getRoutes();
+        List<List<HashMap<String, String>>> result = driverRoutInfo.getRoutes();
         for (int i = 0; i < result.size(); i++) {
 
             // Fetching i-th route
@@ -227,61 +204,54 @@ public class RiderDataCarrier extends AppCompatActivity {
                 double lng = Double.parseDouble(point.get("lng"));
                 LatLng position = new LatLng(lat, lng);
 
-                Location riderOriginLocation= new Location(LocationManager.GPS_PROVIDER);
+                Location riderOriginLocation = new Location(LocationManager.GPS_PROVIDER);
                 riderOriginLocation.setLatitude(originLat);
                 riderOriginLocation.setLongitude(originLong);
 
-                Location riderDestinationLocation= new Location(LocationManager.GPS_PROVIDER);
+                Location riderDestinationLocation = new Location(LocationManager.GPS_PROVIDER);
                 riderDestinationLocation.setLatitude(destinationLat);
                 riderDestinationLocation.setLongitude(destinationLong);
 
-                Location positionOnPath= new Location(LocationManager.GPS_PROVIDER);
+                Location positionOnPath = new Location(LocationManager.GPS_PROVIDER);
                 positionOnPath.setLatitude(position.latitude);
                 positionOnPath.setLongitude(position.longitude);
                 float originDistance = riderOriginLocation.distanceTo(positionOnPath);
                 float destinationDistance = riderDestinationLocation.distanceTo(positionOnPath);
 
-
-
-
-                
-
 //                Toast.makeText(this, "dis "+rr, Toast.LENGTH_SHORT).show();
-                Log.d("myLocationss",""+originDistance+" "+destinationDistance);
-                if(originDistance < radius){
-                    if(originDistance < originFoundPointDistance){
+                Log.d("myLocationss", "" + originDistance + " " + destinationDistance);
+                if (originDistance < radius) {
+                    if (originDistance < originFoundPointDistance) {
                         originFound = true;
                         originFoundPoint = position;
                         originFoundPointDistance = originDistance;
-                        Log.d("FoundLocation","origin path Found");
-                        Toast.makeText(this, "Origin " + originFoundPoint.latitude+" "+originFoundPoint.longitude, Toast.LENGTH_SHORT).show();
+                        Log.d("FoundLocation", "origin path Found");
+                        Toast.makeText(this, "Origin " + originFoundPoint.latitude + " " + originFoundPoint.longitude, Toast.LENGTH_SHORT).show();
 
                     }
 
                 }
-                if(destinationDistance < radius){
-                    if(destinationDistance < destinationFoundPointDistance){
+                if (destinationDistance < radius) {
+                    if (destinationDistance < destinationFoundPointDistance) {
                         destinationFound = true;
                         destinationFoundPoint = position;
                         destinationFoundPointDistance = destinationDistance;
-                        Log.d("FoundLocation","destination path Found");
-                        Toast.makeText(this, "Destination " + destinationFoundPoint.latitude+" "+destinationFoundPoint.longitude, Toast.LENGTH_SHORT).show();
+                        Log.d("FoundLocation", "destination path Found");
+
+                        Toast.makeText(this, "Destination " + destinationFoundPoint.latitude + " " + destinationFoundPoint.longitude, Toast.LENGTH_SHORT).show();
                     }
 
                 }
 
+            }
 
 
-
-                }
-
-
-            if(originFound==true && destinationFound == true){
-                Location originFoundLocation= new Location(LocationManager.GPS_PROVIDER);
+            if (originFound == true && destinationFound == true) {
+                Location originFoundLocation = new Location(LocationManager.GPS_PROVIDER);
                 originFoundLocation.setLatitude(originFoundPoint.latitude);
                 originFoundLocation.setLongitude(originFoundPoint.longitude);
 
-                Location destinationFoundLocation= new Location(LocationManager.GPS_PROVIDER);
+                Location destinationFoundLocation = new Location(LocationManager.GPS_PROVIDER);
                 destinationFoundLocation.setLatitude(destinationFoundPoint.latitude);
                 destinationFoundLocation.setLongitude(destinationFoundPoint.longitude);
 
@@ -292,22 +262,25 @@ public class RiderDataCarrier extends AppCompatActivity {
                 double lng1 = Double.parseDouble(driverOrigin.get("lng"));
 //                LatLng originPosition = new LatLng(lat, lng);
 
-                Location driverOriginLocation= new Location(LocationManager.GPS_PROVIDER);
+                Location driverOriginLocation = new Location(LocationManager.GPS_PROVIDER);
                 driverOriginLocation.setLatitude(lat1);
                 driverOriginLocation.setLongitude(lng1);
-
 
 
                 float riderOriginToDriverOrigin = originFoundLocation.distanceTo(driverOriginLocation);
                 float riderDestinationToDriverOrigin = destinationFoundLocation.distanceTo(driverOriginLocation);
 
 
-                if(riderOriginToDriverOrigin<riderDestinationToDriverOrigin ){
+                if (riderOriginToDriverOrigin < riderDestinationToDriverOrigin) {
 
                     Toast.makeText(this, "PathFound", Toast.LENGTH_SHORT).show();
-                    Log.d("FoundLocation","path Found");
-                }
+                    Log.d("FoundLocation", "path Found");
+                    Log.d("availableDriver", "path Found");
+//                    availableDrivers.add(driverKey);
+                    return driverKey;
+//                    Log.d("availableDriver", "Driver list added item " + availableDrivers.size());
 
+                }
 
 
             }
@@ -315,36 +288,11 @@ public class RiderDataCarrier extends AppCompatActivity {
         }
 
 
+        return null;
     }
 
 
-    private void saveIntoFirebase(){
-
-//        String rider_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
-//        mRiderAvailableRout = FirebaseDatabase.getInstance().getReference("Rider Routs").child(rider_id);
-//
-//        GeoFire geoFire = new GeoFire(mRiderAvailableRout);
-//        geoFire.setLocation("origin", new GeoLocation(originLat, originLong), new GeoFire.CompletionListener() {
-//            @Override
-//            public void onComplete(String key, DatabaseError error) {
-//                if (error != null) {
-//                    System.err.println("There was an error saving the location to GeoFire: " + error);
-//                } else {
-//                    System.out.println("Location saved on server successfully!");
-//                }
-//            }
-//        });
-//        geoFire.setLocation("destination", new GeoLocation(destinationLat, destinationLong), new GeoFire.CompletionListener() {
-//            @Override
-//            public void onComplete(String key, DatabaseError error) {
-//                if (error != null) {
-//                    System.err.println("There was an error saving the location to GeoFire: " + error);
-//                } else {
-//                    System.out.println("Location saved on server successfully!");
-//                }
-//            }
-//        });
-
+    private void saveIntoFirebase() {
 
         mRiderAvailableRout = FirebaseDatabase.getInstance().getReference("Rider Routs");
         String rider_id = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -357,10 +305,9 @@ public class RiderDataCarrier extends AppCompatActivity {
         mRiderAvailableRout.child(rider_id).setValue(locationData).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(RiderDataCarrier.this, "data has been added.", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     Toast.makeText(RiderDataCarrier.this, "Unable to add data", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -379,17 +326,17 @@ public class RiderDataCarrier extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId()==R.id.profile){
+        if (item.getItemId() == R.id.profile) {
 
 
-            startActivity(new Intent(this,Rider_Profile.class));
+            startActivity(new Intent(this, Rider_Profile.class));
 
             return true;
         }
-        if(item.getItemId()==R.id.sign_out){
+        if (item.getItemId() == R.id.sign_out) {
 
             FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this,MainActivity.class));
+            startActivity(new Intent(this, MainActivity.class));
 
             return true;
         }
